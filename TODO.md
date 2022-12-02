dd863021c3771e51ac90b19cd8c2326ddccbea3f
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2022-12-02 17:22:48 +0100

    Modified metadata

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
 # TODO
 
-- [ ] Create calendar object with different name and add events to it (now it is called mlops community, which is not correct)
+- [x] Create calendar object with different name and add events to it (now it is called mlops community, which is not correct)
 - [ ] create github actions to periodically run
--- aggregate.R
+++ aggregate.R
@@ -19,4 +19,26 @@ cleaned_up <-all_events |>
   dplyr::distinct(`DTSTART;TZID=Europe/Amsterdam`, SUMMARY, CREATED, LOCATION, .keep_all = TRUE) |>
   dplyr::arrange(`DTSTART;TZID=Europe/Amsterdam`)
 log_info("found {nrow(cleaned_up)} events")
-ic_write(cleaned_up, "ds_events.ics")
+
+#ic_write(cleaned_up, "ds_events.ics") # I'd like to add some details to the file that do not yet exist
+out <- ic_character(cleaned_up)
+replace_insert <- function(out, what, replacement){
+  start <- grep("BEGIN:VCALENDAR", out)
+  end <- grep("BEGIN:VTIMEZONE", out)
+  loc <- grep(what, out)
+  if(length(loc)>0){
+    if(start < loc && loc < end){
+      out[loc] <- paste0(what, replacement)
+    }
+  }
+  out
+}
+log_info("changing meta information in calendar")
+out |>
+  replace_insert("PRODID:","-//DATA//SCIENCE//MEETUPS//NL") |>
+  replace_insert("X-ORIGINAL-URL:","https://github.com/RMHogervorst/ds_calendar") |>
+  replace_insert("X-WR-CALNAME:","Dutch Data Science Calendar") |>
+  writeLines( "ds_events.ics")
+# PRODID
+# X-ORIGINAL-URL:https://www.meetup.com/amsterdam-mlops-community/events/ical/
+# X-WR-CALNAME:Events - Amsterdam MLOps Community
