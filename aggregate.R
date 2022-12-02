59f34e2a4132324c0f33d89589f3eca389cc5222
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2022-12-02 21:46:47 +0100

    fix bug and simplify calendar parsing

--- aggregate.R
+++ aggregate.R
@@ -1,56 +1,63 @@
 library(calendar)
 library(logger)
 log_threshold(INFO)
-calendars <- read.csv("calendars.csv")
-log_info("found {nrow(calendars)} calendars")
+
+
+#### Functions ###
 # some calendars are empty and this gives a problem.
-parse_calendar <- function(url){
-  #raw <- readLines(url) # this works but is a bit 
-  for (attempt_i in seq_len(10)){
-    resp <- httr::GET(url, httr::user_agent("http://github.com/rmhogervorst/ds_calendar"))
-    if (httr::status_code(resp) == 504){
-      backoff <- runif(n=1, min=0, max=2^attempt_i - 1)
+parse_calendar <- function(url) {
+  #
+  for (attempt_i in seq_len(5)) {
+    res <- curl::curl_fetch_disk(url, tempfile())
+    if (res$status_code == 504) { # deal with timeouts.
+      backoff <- runif(n = 1, min = 0, max = 2^attempt_i - 1)
       log_info("Backing off for {round(backoff,2)} seconds for {url}")
       Sys.sleep(backoff)
-    }else{
-      raw <- httr::content(resp)
+    } else {
+      log_debug("Parsing {url}")
+      if (any(grepl("BEGIN:VEVENT", readLines(res$content, warn = FALSE)))) {
+        result <- ic_read(res$content)
+      } else {
+        log_info("no events in {url}")
+        result <- data.frame()
+      }
       break
     }
   }
-  ic_list(raw)
+  result
 }
 
-
-all_calendar_content<- purrr::map(calendars$address, parse_calendar)
-all_valid_urls <- calendars$address[lengths(all_calendar_content)>0]
-log_info("found {length(all_valid_urls)} groups with events")
-all_events <- purrr::map_dfr(all_valid_urls, ic_read)
-log_debug("found {nrow(all_events)} events")
-if(length(all_valid_urls) == 0){stop('No events found or something went wrong', call. = FALSE)}
-cleaned_up <-all_events |> 
-  dplyr::distinct(`DTSTART;TZID=Europe/Amsterdam`, SUMMARY, CREATED, LOCATION, .keep_all = TRUE) |>
-  dplyr::arrange(`DTSTART;TZID=Europe/Amsterdam`)
-log_info("found {nrow(cleaned_up)} events")
-
-#ic_write(cleaned_up, "ds_events.ics") # I'd like to add some details to the file that do not yet exist
-out <- ic_character(cleaned_up)
-replace_insert <- function(out, what, replacement){
+replace_insert <- function(out, what, replacement) {
   start <- grep("BEGIN:VCALENDAR", out)
   end <- grep("BEGIN:VTIMEZONE", out)
   loc <- grep(what, out)
-  if(length(loc)>0){
-    if(start < loc && loc < end){
+  if (length(loc) > 0) {
+    if (start < loc && loc < end) {
       out[loc] <- paste0(what, replacement)
     }
   }
   out
 }
+
+###### actual work
+calendars <- read.csv("calendars.csv")
+log_info("found {nrow(calendars)} calendars")
+
+all_calendar_content <- purrr::map_dfr(calendars$address, parse_calendar)
+
+if (nrow(all_calendar_content) == 0) {
+  stop("No events found or something went wrong", call. = FALSE)
+}
+cleaned_up <- all_calendar_content |>
+  dplyr::distinct(`DTSTART;TZID=Europe/Amsterdam`, SUMMARY, CREATED, LOCATION, .keep_all = TRUE) |>
+  dplyr::arrange(`DTSTART;TZID=Europe/Amsterdam`)
+log_info("found {nrow(cleaned_up)} events")
+
+# ic_write(cleaned_up, "ds_events.ics") # I'd like to add some details to the file that do not yet exist
+out <- ic_character(cleaned_up)
 log_info("changing meta information in calendar")
 out |>
-  replace_insert("PRODID:","-//DATA//SCIENCE//MEETUPS//NL") |>
-  replace_insert("X-ORIGINAL-URL:","https://github.com/RMHogervorst/ds_calendar") |>
-  replace_insert("X-WR-CALNAME:","Dutch Data Science Calendar") |>
-  writeLines( "ds_events.ics")
-# PRODID
-# X-ORIGINAL-URL:https://www.meetup.com/amsterdam-mlops-community/events/ical/
-# X-WR-CALNAME:Events - Amsterdam MLOps Community
+  replace_insert("PRODID:", "-//DATA//SCIENCE//MEETUPS//NL") |>
+  replace_insert("X-ORIGINAL-URL:", "https://github.com/RMHogervorst/ds_calendar") |>
+  replace_insert("X-WR-CALNAME:", "Dutch Data Science Calendar") |>
+  writeLines("ds_events.ics")
